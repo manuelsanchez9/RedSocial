ab9029a2c099cec7ac7db9ce63b5873d235e469b
Manuel <jefesistemas@masmedellin.com>
2019-03-26 12:59:25 -0500

    26-03-2019

--- RedSocial/main.js
+++ RedSocial/main.js
@@ -1,7 +1,9 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+const express = require('express');
 
+const config = require('./server/config');
 
+const app = config(express());
+
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
